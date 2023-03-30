d2f9daac4b4173d051ce286c2fe997caac452896
CourierID <45730525+CourierID@users.noreply.github.com>
2023-03-29 21:30:31 -0400

    Add resize button for different canvas sizes

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const canvasDiv = document.querySelector(".canvas");
+const resizeButton = document.querySelector("#resizeButton");
 
 console.log(canvasDiv.clientHeight);
 
-let pixelNumber = 16;
-
-
-for (let i = 0; i < pixelNumber; i++) {
-    for (let j = 0; j < pixelNumber; j++) {
-        const pixel = document.createElement("div");
-        pixel.style.width = `${canvasDiv.clientWidth / pixelNumber}px`;
-        pixel.style.height = `${canvasDiv.clientHeight / pixelNumber}px`;
-        canvasDiv.appendChild(pixel);
+function initCanvas(pixelNumber) {
+    for (let i = 0; i < pixelNumber; i++) {
+        for (let j = 0; j < pixelNumber; j++) {
+            const pixel = document.createElement("div");
+            pixel.style.width = `${canvasDiv.clientWidth / pixelNumber}px`;
+            pixel.style.height = `${canvasDiv.clientHeight / pixelNumber}px`;
+            pixel.addEventListener("mouseover", () => {
+                pixel.classList.add('red');
+            });
+            canvasDiv.appendChild(pixel);
+        }
     }
 }
+
+initCanvas(16);
+
+function resetCanvas() {
+    while (canvasDiv.firstChild) {
+        canvasDiv.removeChild(canvasDiv.firstChild);
+    }
+}
+
+
+resizeButton.addEventListener('click', () => {
+    let numPixels = prompt("Enter new amount of pixels! (Less than 100)", "16");
+    if (numPixels > 100) {
+        numPixels = 100;
+    }
+    resetCanvas()
+    initCanvas(numPixels);
+});
